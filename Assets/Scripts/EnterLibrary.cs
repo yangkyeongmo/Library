8fa3ea09fdba0299511aa2284eac845dcba1ff38
yangkyeongmo <yangkyeongmo@snu.ac.kr>
2018-07-13 00:00:00 +0900

    can enter library from 'entry' scene

---
+++ Assets/Scripts/EnterLibrary.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EnterLibrary : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            SceneManager.LoadScene(1);
+        }
+	}
+
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        if(hit.collider.tag == "Door")
+        {
+            SceneManager.LoadScene(1);
+        }
+    }
+}
