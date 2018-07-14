42a735c8d975b872799a45094a8120107301382b
yangkyeongmo <yangkyeongmo@snu.ac.kr>
2018-07-14 14:36:01 +0900

    activate or deactiavte commandLine

--- Assets/Scripts/CommandScript.cs
+++ Assets/Scripts/CommandScript.cs
@@ -1,16 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CommandScript : MonoBehaviour {
 
+    public GameObject commandLine;
+    public Text commandText;
+
 	// Use this for initialization
 	void Start () {
-		
+        
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        if (Input.GetKey(KeyCode.F11)){
+            if (!commandLine.activeSelf)
+            {
+                commandLine.SetActive(true);
+                DealWithCommand();
+            }
+            else
+            {
+                commandLine.SetActive(false);
+            }
+        }
 	}
+
+    void DealWithCommand()
+    {
+
+    }
 }
